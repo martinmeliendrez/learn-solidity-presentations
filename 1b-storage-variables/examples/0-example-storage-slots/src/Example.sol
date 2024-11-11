b532029d9d226e2acc12654f103f449c8d98d626
Martin Meliendrez <martinmeliendrez@gmail.com>
2024-11-11 16:01:36 -0300

    Storage variables

--- 1b-storage-variables/examples/0-example-storage-slots/src/Example.sol
+++ 1b-storage-variables/examples/0-example-storage-slots/src/Example.sol
@@ -4,17 +4,18 @@ pragma solidity ^0.8.20;
 import "forge-std/console.sol";
 
 contract Example {
-    uint256 constant a = 55;
-    uint256 b; // storage slot - 0x0
+    uint256 a = 100;// storage slot - 0x0
+    uint256 b = type(uint256).max; // storage slot - 0x1
     bool c; // storage slot - 0x1
 
     constructor() {
         // SSTORE - store to some storage location
         // SLOAD - read from some storage location
-        bool x;
+        bytes32 x;
         assembly {
-            x := sload(0x2)
+            x := sload(0x1)
         }
-        console.log(x);
+        
+        console.logBytes32(x);
     }
 }
