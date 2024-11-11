e29775788ad41b3f780cf46d6ebd186614a11cef
Martin Meliendrez <martinmeliendrez@gmail.com>
2024-11-11 16:41:04 -0300

    Using External View function

--- 1c-functions/examples/0-returning-values/src/Example.sol
+++ 1c-functions/examples/0-returning-values/src/Example.sol
@@ -6,12 +6,22 @@ import "forge-std/console.sol";
 contract Example {
     uint256 public sum;
     uint256 public product;
+    uint256 public x;
 
-    constructor(uint256 x, uint256 y) {
-        (sum, product) = math(x, y);
+    constructor(uint256 _x, uint256 _y) {
+        (sum, product) = math(_x, _y);
+        x = _x;
     }
 
-    function math(uint256 x, uint256 y) private pure returns (uint256, uint256) {
-        return (x + y, x * y);
+    function math(uint256 _x, uint256 _y) private pure returns (uint256, uint256) {
+        return (_x + _y, _x * _y);
+    }
+
+    function increment() external {
+        x += 1;
+    }
+
+    function add(uint256 y) external view returns(uint256) {
+        return x+y;
     }
 }
