fd72e0e00315e86931e4e96216acf84832acb95d
Martin Meliendrez <martinmeliendrez@gmail.com>
2024-11-16 11:44:48 -0300

    LEARNING REVERT - Only Owner

--- 3-reverting-transactions/examples/0-revert-require/src/Practice.sol
+++ 3-reverting-transactions/examples/0-revert-require/src/Practice.sol
@@ -1,12 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+
+// Goal 1: Require 1 Ether
+// Add a payable constructor method that requires a 1 ether deposit.
+
+// Goal 2: Owner Withdrawal
+// 1. Create a public function withdraw that will withdraw all funds from the contract and send them to the deployer of the contract.
+// 2. Require that only the deployer of the contract be allowed to call this function. For all other addresses, this function should revert.
+
 contract Contract {
+    address deployer = msg.sender;
+
+    // @notice a non-privileged user attempted to access deployer-only method
+    error NotContractDeployer(address);
 
     // Payable contructor to accept and require a minimum of 1 ether
     constructor() payable {
         // Require at least 1 ether to be sent; otherwise, revert the transaction
         require(msg.value >= 1 ether, "Insufficient Ether provided");
+    }
+
+    function withdraw() external {
+        if(msg.sender != deployer) {
+            // Revert with a custom error revert 
+            revert NotContractDeployer(msg.sender);
+        }
+        //  Withdraw all funds from the contract and send them to the deployer of the contract
+        (bool success, ) = deployer.call{ value: address(this).balance }("");
+        require(success);
     }
     
 }
--- 3-reverting-transactions/examples/0-revert-require/test/Practice.t.sol
+++ 3-reverting-transactions/examples/0-revert-require/test/Practice.t.sol
@@ -6,14 +6,34 @@ import "../src/Practice.sol";
 
 contract ContractTest is Test {
     Contract public myContract;
+    address owner = address(2);
 
-    function testSuccess() public {
+    function setUp() public {
+        hoax(owner);
+        myContract = new Contract{ value: 1 ether }();
+    }
+
+    function testSuccess1Ether() public {
         myContract = new Contract{ value: 1 ether }();
         assertFalse(address(myContract) == address(0));
     }
 
-    function testFailure() public {
+    function testFailure1Ether() public {
         myContract = new Contract{ value: 0.5 ether }();
         assertEq(address(myContract), address(0));
+    }
+
+    function testAsOwner() public {
+        vm.prank(owner);
+        uint balanceBefore = address(owner).balance;
+        myContract.withdraw();
+        uint balanceAfter = address(owner).balance;
+        assertEq(balanceAfter - balanceBefore, 1 ether);
+    }
+
+    function testAsNotOwner() public {
+        vm.prank(address(3));
+        vm.expectRevert();
+        myContract.withdraw();
     }
 }
